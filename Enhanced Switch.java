aaeb9b76bf70ee840efdb6978f75257bd8576cd1
Rajnil Saha <rajnilhere@gmail.com>
2025-07-03 15:57:52 +0530

    Create Enhanced Switch.java

---
+++ Enhanced Switch.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class a6
+{ 
+  //Enhanced switch using arrow operator ->
+  public static void main(String[] args) {
+    String day;
+    Scanner sc=new Scanner(System.in);
+    System.out.print("Enter the day: ");
+    day=sc.nextLine();
+    switch(day) 
+    {
+      case "Monday" , "Tuesday" , "Wednesday" , "Thursday" , "Friday"  -> // using of arrow operator
+       System.out.println("It is a weekday"); // A java 14 feature that reduces redundancy 
+      case "Saturday" , "Sunday" ->
+       System.out.println("It is a weekend");
+      default ->  // For invalid input 
+      System.out.println("It is not a day");
+
+    }
+    sc.close();
+
+  }
+}
