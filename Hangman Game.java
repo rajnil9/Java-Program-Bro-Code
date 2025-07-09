15661c616774a15c84df5b0ad95b8a2cfd6957b4
Rajnil Saha <rajnilhere@gmail.com>
2025-07-09 15:30:59 +0530

    Update Hangman Game.java

--- Hangman Game.java
+++ Hangman Game.java
@@ -1,3 +1,108 @@
-public class Hangman Game {
+import java.util.*;
+class Hangman
+{
+  public static void main(String[] args) {
+    
+    String word="pizza";
+
+    Scanner sc=new Scanner(System.in);
+    ArrayList<Character> wordState= new ArrayList<>();
+    int wrongguess=0;
+
+
+    for(int i=0;i<word.length();i++)
+    {
+      wordState.add('_');
+    }
+    
+    System.out.println("************************");
+    System.out.println("Welcome to Java Hangman!");
+    System.out.println("************************");
+
+   while(wrongguess<6)
+   {
+    System.out.println(HangmanArt(wrongguess));
+      
+    System.out.print("Word: ");
+
+    for(char c: wordState)
+    {
+      System.out.print(c+ "  ");
+    }
+    System.out.println();
+
+    System.out.print("Guess a character: ");
+    char Guess=sc.next().toLowerCase().charAt(0);
+
+    if(word.indexOf(Guess)>=0)
+    {
+      
+    System.out.println("Correct Guess");
+    for(int i=0;i<word.length();i++)
+    {
+      if(word.charAt(i)==Guess)
+      word.set(i,Guess);
+
+    }
+
+
+    }
+    else
+    wrongguess++;
+    System.out.println("Wrong Guess");
+
+   }
+
+    
+
+    sc.close();
+  }
+
+
+  static String HangmanArt(int wrongguess)
+  {
+    return switch(wrongguess)
+    {
+      case 0 -> """
+          
+
+
+          """;
+
+      case 1 -> """
+                o
+
+
+          """;
+       case 2 -> """
+                    o
+                    |
+
+         """;
+       case 3-> """
+                    o
+                   /|
+
+          """;
+       case 4 -> """
+                    o
+                   /|\\
+
+          """;
+      case 5 -> """
+                    o
+                   /|\\
+                   /
+          """;
+      case 6-> """
+                    o
+                   /|\\
+                   / \\
+          """;
+       
+       default -> "";
+    };
+  }
+
   
 }
