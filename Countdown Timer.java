8e1fa10521098b4044a35147414419e34a386341
Rajnil Saha <rajnilhere@gmail.com>
2025-07-10 01:42:07 +0530

    Update Countdown Timer.java

--- Countdown Timer.java
+++ Countdown Timer.java
@@ -6,9 +6,10 @@ class timer // Class name
     {
         Scanner sc = new Scanner(System.in); // Create Scanner object for user input
 
-        System.out.print("Enter the number for Countdown: ");
+        System.out.print("Enter the number of seconds for Countdown(must be positive): ");
         int res = sc.nextInt(); // Read countdown starting number from the user
-
+        if(res>0)
+        {
         Timer timer = new Timer(); // Create a Timer object to schedule the countdown task
 
         // Create a TimerTask with overridden run() method
@@ -24,7 +25,7 @@ class timer // Class name
 
                 if (c < 0) // When countdown reaches below zero
                 {
-                    System.out.println("HAPPY NEW YEAR"); // Print celebration message
+                    System.out.println("HAPPY NEW YEAR!"); // Print celebration message
                     timer.cancel(); // Stop the Timer to end the program
                 }
             }
@@ -34,5 +35,10 @@ class timer // Class name
         // - initial delay of 0 ms
         // - repeat period of 500 ms (0.5 seconds)
         timer.schedule(t, 0, 500);
+         }
+         else
+         {
+         System.out.println("Enter number of seconds for Countdown can't be nagative! Try again!");
+         }
     }
 }
