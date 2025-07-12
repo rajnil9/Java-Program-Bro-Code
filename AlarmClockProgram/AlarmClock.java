b3d7974f75fec05940fec9ef9440226fad68eb60
Rajnil Saha <rajnilhere@gmail.com>
2025-07-12 23:32:35 +0530

    Create AlarmClock.java

---
+++ AlarmClockProgram/AlarmClock.java
@@ -0,0 +1,34 @@
+package AlarmClockProgram;
+import java.time.LocalTime;
+public class AlarmClock implements Runnable 
+{
+  private final LocalTime alarmTime;
+
+  AlarmClock(LocalTime alarmTime)
+  {
+    this.alarmTime=alarmTime;
+  }
+
+  @Override
+  public void run() {
+    
+    while(LocalTime.now().isBefore(alarmTime))
+    {
+        try
+        {
+          Thread.sleep(1000);
+
+          LocalTime now= LocalTime.now();
+          System.out.printf("\r%02d:02d:02d\n" , now.getHour(), now.getMinute(), now.getSecond());
+        } 
+        catch(InterruptedException e)
+        {
+            System.out.println("Thread was interrupted");
+        }
+    }
+
+    System.out.println("\nALARM NOISES");
+    
+  }
+  
+}
